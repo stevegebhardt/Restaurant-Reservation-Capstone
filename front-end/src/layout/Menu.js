af39fae397d20aa8bfd2f417a9dc41c993ce693d
stevegebhardt <gebhardt.steven3@gmail.com>
2024-01-25 12:35:06 -0500

    eating tables is the way to go

--- front-end/src/layout/Menu.js
+++ front-end/src/layout/Menu.js
@@ -17,7 +17,7 @@ function Menu() {
           to="/"
         >
           <div className="sidebar-brand-text mx-3">
-            <span>RESTaurant Reservations</span>
+            <span>Eating Tables</span>
           </div>
         </Link>
         <hr className="sidebar-divider my-0" />
